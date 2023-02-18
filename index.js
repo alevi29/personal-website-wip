2af17c2dea946351d6337d080f35b0379f2f3025
alevi29 <aca277@sfu.ca>
2023-02-18 09:56:31 -0800

    commit

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const navToggle = document.querySelector(".nav-toggle");
+const progessBar = document.querySelector(".loading");
+
+function loadComplete() {
+    document.querySelector(".preloader").classList.add("loadingHidden");
+}
+
+updateProg(progessBar, 100);
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
+
+function updateProg(progress, percent) {
+    percent = Math.round(percent);
+    var cur = 0;
+    while (cur <= percent) {
+        progress.querySelector(".loadingFill").style.width = `${cur}%`;
+        progress.querySelector(".loadingText").innerHTML = `${cur}%`;
+        cur++;
+    }
+}
